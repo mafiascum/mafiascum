40ae6cd6d5822085c92b9e50881c5ea29b3072c9
Michael Mason <mikemason930@gmail.com>
2013-04-27 05:26:06 +0000

    * Require that the user be logged in to process the Heartbeat packet.

--- java/SiteChatServer/src/net/mafiascum/web/sitechat/server/inboundpacket/operator/SiteChatInboundHeartbeatPacketOperator.java
+++ java/SiteChatServer/src/net/mafiascum/web/sitechat/server/inboundpacket/operator/SiteChatInboundHeartbeatPacketOperator.java
@@ -1,20 +1,16 @@
 package net.mafiascum.web.sitechat.server.inboundpacket.operator;
 
-import net.mafiascum.util.MiscUtil;
 import net.mafiascum.web.sitechat.server.SiteChatServer;
 import net.mafiascum.web.sitechat.server.SiteChatServer.SiteChatWebSocket;
 import net.mafiascum.web.sitechat.server.SiteChatUser;
-import net.mafiascum.web.sitechat.server.SiteChatUtil;
-import net.mafiascum.web.sitechat.server.conversation.SiteChatConversation;
-import net.mafiascum.web.sitechat.server.conversation.SiteChatConversationWithUserList;
-import net.mafiascum.web.sitechat.server.inboundpacket.SiteChatInboundConnectPacket;
-
-import com.google.gson.Gson;
 
 public class SiteChatInboundHeartbeatPacketOperator implements SiteChatInboundPacketOperator {
 
   public void process(SiteChatServer siteChatServer, SiteChatWebSocket siteChatWebSocket, String siteChatInboundPacketJson) throws Exception {
     SiteChatUser siteChatUser = siteChatWebSocket.getSiteChatUser();
-    siteChatServer.updateUserActivity(siteChatUser.getId());
+    
+    if(siteChatUser != null) {
+      siteChatServer.updateUserActivity(siteChatUser.getId());
+    }
   }
 }
