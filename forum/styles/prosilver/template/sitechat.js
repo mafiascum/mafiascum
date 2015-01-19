16a0639ea8ed227f8b9fdb99b9ade7105c5aab77
Michael Mason <mikemason930@gmail.com>
2015-01-19 00:31:11 +0000

    Fixed a bug that allowed users to inject JavaScript into chat room names.

--- forum/styles/prosilver/template/sitechat.js
+++ forum/styles/prosilver/template/sitechat.js
@@ -653,7 +653,11 @@ function Client() {
 		if (client.rooms) {
 			for (var room in client.rooms) {
 				if (client.rooms[room] !== undefined && client.rooms[room] !== null){
-					$("#roomstab").append('<div id="chatroom' + client.rooms[room].id + '"><div class="roomtitle"><span class="expand-icon">' + (client.rooms[room].expanded ? '-' : "+") + '</span>' + client.rooms[room].name + '<span class="usercount">(' + client.rooms[room].userIdSet.length + ')</span><a href="#" class="joinroom hidden">Join Room</a></span></div><div class="userlist"' + (client.rooms[room].expanded ? '' : "style='display:none;'") + '></div></div>');
+
+					var $roomTab = $('<div id="chatroom' + client.rooms[room].id + '"><div class="roomtitle"><span class="expand-icon">' + (client.rooms[room].expanded ? '-' : "+") + '</span><span class="roomName"></span><span class="usercount">(' + client.rooms[room].userIdSet.length + ')</span><a href="#" class="joinroom hidden">Join Room</a></span></div><div class="userlist"' + (client.rooms[room].expanded ? '' : "style='display:none;'") + '></div></div>');
+					$roomTab.find(".roomName").text(client.rooms[room].name);
+					$("#roomstab").append($roomTab);
+
 					var identifier = '#chatroom' + client.rooms[room].id + ' .userlist';
 					$(identifier).append('<ul>');
 					for (var k = 0; k < client.rooms[room].userIdSet.length; k++){
